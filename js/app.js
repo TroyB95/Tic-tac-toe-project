3bfff64ab8b3ddad3387c99e3ed60e44e9912e46
TroyB <troy_budd95@msn.com>
2018-05-16 10:28:17 +0100

    Created base player object and created variables for the selector buttons

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const xSelector = document.querySelector('.x-selector');
+const ySelector = document.querySelector('.y-selector');
+
+xSelector.addEventListener('click', function(){
+	let player1.symbol = 'x';
+})
+
+ySelector.addEventListener('click', function(){
+	let player1.symbol = 'y';
+})
+
+var Player = function(score) {
+	this.symbol = symbol;
+	this.score = score;
+};
+
+let player1 = new Player();
+
+
+let player2 = new Player();
