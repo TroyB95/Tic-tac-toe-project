e111d82c7fa26d49c03bb361067c7862ef8cfb8b
TroyB <troy_budd95@msn.com>
2018-05-25 08:50:58 +0100

    Base game is almost complete, will be updating from now on

--- js/app.js
+++ js/app.js
@@ -1,20 +1,269 @@
-const xSelector = document.querySelector('.x-selector');
-const ySelector = document.querySelector('.y-selector');
+/* ======= Model ======= */
 
-xSelector.addEventListener('click', function(){
-	let player1.symbol = 'x';
-})
+// Data for each of the players, plus currentTurn to track what turn it is.
 
-ySelector.addEventListener('click', function(){
-	let player1.symbol = 'y';
-})
-
-var Player = function(score) {
-	this.symbol = symbol;
-	this.score = score;
+var model = {
+	currentTurn: 0,
+	players: [
+		{
+			symbol: '',
+			score: 0,
+			name: 'Player1'
+		},
+		{
+			symbol: '',
+			score: 0,
+			name: 'Player2'
+		}
+		]
 };
 
-let player1 = new Player();
 
 
-let player2 = new Player();
+/* ======= Octopus ======= */
+
+var octopus = {
+
+	init: function() {
+		model.currentTurn = 0;
+
+		symbolSelectView.init();
+		refreshButton.init();
+	},
+
+	winCondition: function() {
+		var cells = document.querySelectorAll('.cell');
+
+		// Win conditions for player 1. Very verbose
+
+		if(cells[0].innerHTML === model.players[0].symbol && cells[1].innerHTML === model.players[0].symbol && cells[2].innerHTML === model.players[0].symbol) {
+			alert('Player 1, ' + model.players[0].symbol + ' Wins!');
+			model.players[0].score++;
+			scoreView.init();
+			this.gridReset();
+		} else if(cells[0].innerHTML === model.players[0].symbol && cells[3].innerHTML === model.players[0].symbol && cells[6].innerHTML === model.players[0].symbol) {
+			alert('Player 1, ' + model.players[0].symbol + ' Wins!');
+			model.players[0].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[0].innerHTML === model.players[0].symbol && cells[4].innerHTML === model.players[0].symbol && cells[8].innerHTML === model.players[0].symbol) {
+			alert('Player 1, ' + model.players[0].symbol + ' Wins!');
+			model.players[0].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[1].innerHTML === model.players[0].symbol && cells[4].innerHTML === model.players[0].symbol && cells[7].innerHTML === model.players[0].symbol) {
+			alert('Player 1, ' + model.players[0].symbol + ' Wins!');
+			model.players[0].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[2].innerHTML === model.players[0].symbol && cells[5].innerHTML === model.players[0].symbol && cells[8].innerHTML === model.players[0].symbol) {
+			alert('Player 1, ' + model.players[0].symbol + ' Wins!');
+			model.players[0].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[2].innerHTML === model.players[0].symbol && cells[4].innerHTML === model.players[0].symbol && cells[6].innerHTML === model.players[0].symbol) {
+			alert('Player 1, ' + model.players[0].symbol + ' Wins!');
+			model.players[0].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[3].innerHTML === model.players[0].symbol && cells[4].innerHTML === model.players[0].symbol && cells[5].innerHTML === model.players[0].symbol) {
+			alert('Player 1, ' + model.players[0].symbol + ' Wins!');
+			model.players[0].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[6].innerHTML === model.players[0].symbol && cells[7].innerHTML === model.players[0].symbol && cells[8].innerHTML === model.players[0].symbol) {
+			alert('Player 1, ' + model.players[0].symbol + ' Wins!');
+			model.players[0].score++;
+			scoreView.init();
+			this.gridReset();
+
+		// Win condition for player 2.
+
+		} else if(cells[0].innerHTML === model.players[1].symbol && cells[1].innerHTML === model.players[1].symbol && cells[2].innerHTML === model.players[1].symbol) {
+			alert('Player 2, ' + model.players[1].symbol + ' Wins!');
+			model.players[1].score++;
+			scoreView.init();
+			this.gridReset();
+		} else if(cells[0].innerHTML === model.players[1].symbol && cells[3].innerHTML === model.players[1].symbol && cells[6].innerHTML === model.players[1].symbol) {
+			alert('Player 2, ' + model.players[1].symbol + ' Wins!');
+			model.players[1].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[0].innerHTML === model.players[1].symbol && cells[4].innerHTML === model.players[1].symbol && cells[8].innerHTML === model.players[1].symbol) {
+			alert('Player 2, ' + model.players[1].symbol + ' Wins!');
+			model.players[1].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[1].innerHTML === model.players[1].symbol && cells[4].innerHTML === model.players[1].symbol && cells[7].innerHTML === model.players[1].symbol) {
+			alert('Player 2, ' + model.players[1].symbol + ' Wins!');
+			model.players[1].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[2].innerHTML === model.players[1].symbol && cells[5].innerHTML === model.players[1].symbol && cells[8].innerHTML === model.players[1].symbol) {
+			alert('Player 2, ' + model.players[1].symbol + ' Wins!');
+			model.players[1].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[2].innerHTML === model.players[1].symbol && cells[4].innerHTML === model.players[1].symbol && cells[6].innerHTML === model.players[1].symbol) {
+			alert('Player 2, ' + model.players[1].symbol + ' Wins!');
+			model.players[1].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[3].innerHTML === model.players[1].symbol && cells[4].innerHTML === model.players[1].symbol && cells[5].innerHTML === model.players[1].symbol) {
+			alert('Player 2, ' + model.players[1].symbol + ' Wins!');
+			model.players[1].score++;
+			scoreView.init();
+			this.gridReset();
+		}  else if(cells[6].innerHTML === model.players[1].symbol && cells[7].innerHTML === model.players[1].symbol && cells[8].innerHTML === model.players[1].symbol) {
+			alert('Player 2, ' + model.players[1].symbol + ' Wins!');
+			model.players[1].score++;
+			scoreView.init();
+			this.gridReset();
+		} else if(cells[0].innerHTML !== '' && cells[1].innerHTML !== '' && cells[2].innerHTML !== '' && cells[3].innerHTML !== '' && cells[4].innerHTML !== '' && cells[5].innerHTML !== '' && cells[6].innerHTML !== '' && cells[7].innerHTML !== '' && cells[8].innerHTML !== '') {
+			this.gridReset();
+		}
+	},
+
+	// Reset function when a player wins
+	gridReset: function() {
+		var cells = document.querySelectorAll('.cell');
+		setTimeout(function() {
+			cells.forEach(function(cell){
+			cell.innerHTML = '';
+			model.currentTurn = 0;
+			if(cell.classList.contains('remove-events')){
+				cell.classList.toggle('remove-events');
+			}
+		})
+		}, 1500);
+		
+	},
+
+	gridRestart: function() {
+
+		var cells = document.querySelectorAll('.cell');
+			cells.forEach(function(cell){
+			cell.innerHTML = '';
+			if(cell.classList.contains('remove-events')){
+				cell.classList.toggle('remove-events');
+			}
+			model.currentTurn = 0;
+			model.players[0].score = 0;
+			model.players[1].score = 0;
+			scoreView.init();
+		})
+		
+	}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+/* ======= View ======= */
+
+var symbolSelectView = {
+
+	init: function() {
+		var xSelector = document.querySelector('.x-selector');
+		var oSelector = document.querySelector('.o-selector');
+
+		xSelector.addEventListener('click', function(){
+			if(model.players[0].symbol === '' && model.players[1].symbol === '') {
+				model.players[0].symbol = 'X';
+				model.players[1].symbol = 'O'
+				gameBoardView.init();
+		} else {
+			alert('X is already selected to go first');
+		}
+		})
+
+		oSelector.addEventListener('click', function(){
+			if(model.players[0].symbol === '' && model.players[1].symbol === '') {
+				model.players[0].symbol = 'O';
+				model.players[1].symbol = 'X';
+				gameBoardView.init();
+			} else {
+				alert('O is already selected to go first');
+			}
+		})
+	},
+
+	
+		
+	
+
+};
+
+
+var gameBoardView = {
+
+	init: function() {
+		this.board = document.querySelector('.inside-board');
+
+		this.render();
+	},
+
+	render: function() {
+		// When a user clicks a cell, change its background color
+		this.board.addEventListener("mousedown", function(e) {
+			if(e.button === 0 && model.currentTurn === 0) {
+  				if (e.target && e.target.nodeName == "LI") {
+    				e.target.innerHTML = model.players[0].symbol;
+    				e.target.classList.toggle('remove-events');
+    				model.currentTurn++;
+    				octopus.winCondition();
+  				}
+  			} else if (e.button === 0 && model.currentTurn === 1) {
+  				if (e.target && e.target.nodeName == "LI") {
+    				e.target.innerHTML = model.players[1].symbol;
+    				e.target.classList.toggle('remove-events');
+    				model.currentTurn = 0;
+    				octopus.winCondition();
+
+  				}
+  			}
+  		});
+	}
+}
+
+var scoreView = {
+
+	init: function() {
+		this.playerOne = document.querySelector('.player-one');
+		this.playerTwo = document.querySelector('.player-two');
+
+		this.render();
+	},
+
+	render: function() {
+		this.playerOne.innerHTML = 'Player 1: ' + model.players[0].score;
+		this.playerTwo.innerHTML = 'Player 2: ' + model.players[1].score;
+	}
+}
+
+var refreshButton = {
+
+	init: function() {
+		this.refreshBtn = document.querySelector('.refresh-icon');
+
+		this.render();
+	},
+
+
+	render: function() {
+		this.refreshBtn.addEventListener('click', function() {
+				octopus.gridRestart();
+		})
+	}
+}
+
+
+octopus.init();
